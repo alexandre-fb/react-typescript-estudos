065b7cbb4066b89d17b7af4aa1e9dd2c58d1d462
alexandre-fb <alexandrefb.86@gmail.com>
2024-07-16 23:49:14 -0300

    Add components

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import React from "react";
 import { Button } from "./Button";
+import { Input } from "./Input";
 
 function App() {
   const [total, setTotal] = React.useState(0);
+  const [data, setData] = React.useState('');
 
   function incrementar(event: React.MouseEvent<HTMLButtonElement>) {
     setTotal(total + 1);
@@ -11,9 +13,15 @@ function App() {
   return (
     <div>
       <p>{total}</p>
-      <Button tamanho="1.5rem" onClick={ incrementar }>
+      <Button tamanho="1.5rem" onClick={ incrementar } className="teste">
         Incrementar
       </Button>
+
+      <p>Início da viagem: { data }</p>
+      <Input id="nome" label="Nome" type="text" />
+      <Input id="email" label="Email" type="email"/>
+      <Input id="data" label="Data" type="date" onChange={(event) => setData(event.currentTarget.value) } />
+      <Input id="horario" label="Horário" type="time" />
     </div>
   );
 }
--- src/Button.tsx
+++ src/Button.tsx
@@ -1,15 +1,30 @@
-import React from 'react'
+import React from "react";
 
-type ButtonProps = {
-  children?: React.ReactNode;
-  onClick?: () => void;
+//defininfo o children manualmente
+// type ButtonProps = {
+//   children?: React.ReactNode;
+//   onClick?: () => void;
+//   tamanho?: string;
+// }
+
+//definindo o children automaticamente
+// type ButtonProps2 = React.PropsWithChildren<{
+//   onClick?: () => void;
+//   tamanho?: string;
+// }>
+
+//QUando tem um só elemeneto, pode usar essa propriedade, que pode passar qualquer propriedade da tag button direto para o componente Button, como onClick ou className, por exemplo
+type ButtonProps = React.ComponentProps<"button"> & {
   tamanho?: string;
-}
+};
 
-export const Button = (props: ButtonProps) => {
+export const Button = ({ tamanho, children, ...props }: ButtonProps) => {
   return (
-    <button style={{ fontSize: props.tamanho }} onClick={ props.onClick }>
-      { props.children }
-      </button>
-  )
-}
+    <button
+      style={{ fontSize: tamanho }}
+      {...props}
+    >
+      {children}
+    </button>
+  );
+};
