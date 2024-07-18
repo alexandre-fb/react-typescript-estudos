8efb0b38c91d7811c805ff995eb6ebbd253d6366
alexandre-fb <alexandrefb.86@gmail.com>
2024-07-17 23:53:16 -0300

    ADd checkbox e UseState

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Button } from "./Button";
 import { Input } from "./Input";
+import { Checkbox } from "./Checkbox";
+import { TsUseEffect } from "./TsUseState";
 
 function App() {
   const [total, setTotal] = React.useState(0);
@@ -13,15 +15,24 @@ function App() {
   return (
     <div>
       <p>{total}</p>
+
+      {/* Button */}
       <Button tamanho="1.5rem" onClick={ incrementar } className="teste">
         Incrementar
       </Button>
 
+      {/* Input */}
       <p>Início da viagem: { data }</p>
       <Input id="nome" label="Nome" type="text" />
       <Input id="email" label="Email" type="email"/>
       <Input id="data" label="Data" type="date" onChange={(event) => setData(event.currentTarget.value) } />
       <Input id="horario" label="Horário" type="time" />
+
+      {/* Checkbox */}
+      <Checkbox label="Aceito" />
+
+      {/* UseState */}
+      <TsUseEffect />
     </div>
   );
 }
